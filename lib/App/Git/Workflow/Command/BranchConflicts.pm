b1744f4dfd07b6a77c50b664558e5d018cd172fe
Ivan Wills <ivan.wills@gmail.com>
2015-04-12 10:01:00 +1000

    Initial release

---
+++ lib/App/Git/Workflow/Command/BranchConflicts.pm
@@ -0,0 +1,93 @@
+package App::Git::Workflow::Command::BranchConflicts;
+
+# Created on: 2015-04-12 06:56:18
+# Create by:  Ivan Wills
+# $Id$
+# $Revision$, $HeadURL$, $Date$
+# $Revision$, $Source$, $Date$
+
+use strict;
+use warnings;
+use Pod::Usage ();
+use Data::Dumper qw/Dumper/;
+use English qw/ -no_match_vars /;
+use App::Git::Workflow;
+use App::Git::Workflow::Command qw/get_options/;
+
+our $VERSION  = 0.8;
+our $workflow = App::Git::Workflow->new;
+our ($name)   = $PROGRAM_NAME =~ m{^.*/(.*?)$}mxs;
+our %option;
+
+sub run {
+    my ($self) = @_;
+
+    get_options(
+        \%option,
+        'quiet|q',
+    );
+
+
+    return;
+}
+
+
+1;
+
+__DATA__
+
+=head1 NAME
+
+App::Git::Workflow::Command::BranchConflicts - <One-line description of module's purpose>
+
+=head1 VERSION
+
+This documentation refers to App::Git::Workflow::Command::BranchConflicts version 0.0.1
+
+=head1 SYNOPSIS
+
+   use App::Git::Workflow::Command::BranchConflicts;
+
+   # Brief but working code example(s) here showing the most common usage(s)
+   # This section will be as far as many users bother reading, so make it as
+   # educational and exemplary as possible.
+
+
+=head1 DESCRIPTION
+
+
+=head1 SUBROUTINES/METHODS
+
+
+=head1 DIAGNOSTICS
+
+=head1 CONFIGURATION AND ENVIRONMENT
+
+=head1 DEPENDENCIES
+
+=head1 INCOMPATIBILITIES
+
+=head1 BUGS AND LIMITATIONS
+
+There are no known bugs in this module.
+
+Please report problems to Ivan Wills (ivan.wills@gmail.com).
+
+Patches are welcome.
+
+=head1 AUTHOR
+
+Ivan Wills - (ivan.wills@gmail.com)
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright (c) 2015 Ivan Wills (14 Mullion Close, Hornsby Heights, NSW Australia 2077).
+All rights reserved.
+
+This module is free software; you can redistribute it and/or modify it under
+the same terms as Perl itself. See L<perlartistic>.  This program is
+distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.
+
+=cut
